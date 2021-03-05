8f4aefd120d35b767f3d62a57dcf7628173038ef
khokon9363 <khokon9363@gmail.com>
2021-03-05 16:43:24 +0600

    Doctorseba Web View using native-script-vue

---
+++ README.md
@@ -0,0 +1,1 @@
+## This is an Web View App by Me (Hridoy) using native-script-vue
---
+++ app/components/Home.vue
@@ -0,0 +1,24 @@
+<template>
+    <Page actionBarHidden="false">
+        <ActionBar>
+            <Label text="Doctorseba24"/>
+        </ActionBar>
+        <StackLayout backgroundColor="#3c495e">
+            <WebView src="http://doctorseba.net" />
+            <WebView src="~/html" />
+            <WebView src="<div><h1>This is Webview by Hridoy</h1></div>" />
+        </StackLayout>
+    </Page>
+</template>
+
+<script>
+  export default {
+    
+  };
+</script>
+
+<style scoped>
+    Label{
+            font-size: 20px;
+        }
+</style>
